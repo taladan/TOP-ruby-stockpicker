6dc83bf86a0b07ea2648b3ad2eb4dc3617e28ac0
D.J. Crosby <taladan@gmail.com>
2023-03-08 10:47:21 -0500

    Get best buy/sell dates from array of integers
    Stock prices are calculated as integers for the purposes of this
    exercise:  We split off the stock array into an array of high
    values and an array of low values.  We then iterate through the high
    and the low values looking for values that meet the criteria:
      - Is high-low larger than our current max and
      - is the index of high greater than the index of the low
    once we hit both conditions, we pack the values of the current high
    and the current low and return the indices of each from the original
    passed list as an array printed to the cli

--- stock_pick.rb
+++ stock_pick.rb
@@ -1,6 +1,28 @@
 # stock_pick.rb
 
 stocks = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+stocks2 = [534, 4, 8, 1, 254, 82, 365]
 
 def stock_picker(stocks)
+  sorted_stocks = stocks.sort.uniq
+  lows = sorted_stocks.slice(0..sorted_stocks.length / 2)
+  highs = sorted_stocks.slice(sorted_stocks.length / 2..-1).reverse
+
+  current_max = 0
+  current_high = 0
+  highs.each do |high|
+    lows.each do |low|
+      high_index = stocks.find_index(high)
+      low_index = stocks.find_index(low)
+      if high - low > current_max && high_index > low_index
+        current_max = high - low
+        current_high = high
+        current_low = low
+        p [stocks.find_index(current_low), stocks.find_index(current_high)]
+      end
+    end
+  end
 end
+
+stock_picker(stocks)
+stock_picker(stocks2)
